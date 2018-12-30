c9e7553d12c02de0cbdd214e285fe1b62e2d710e
Ly Cao <lycao@10-23-0-196.dynapool.nyu.edu>
2018-12-30 15:20:05 -0500

    added production port

--- server/server.js
+++ server/server.js
@@ -4,21 +4,10 @@ const express = require('express');
 
 //app.use publicPath
 const  publicPath = path.join(__dirname, '../public');
-
-
-
-console.log(__dirname + '/../public');//shows going into and out of /../
-console.log(publicPath);
-
-//path is a built-in module, doesnt have to install via npm
-
-//static middleware, configure, create new express app
-//port.listen 3000
-//console.log(server is up )
-//start server in terminal, => make server runs on port 3000
-
-var port = 3000;
+const port = process.env.PORT || 3000;
 var app = express();
+
+
 app.use(express.static(publicPath));
 
 app.listen(port, ()=>{
